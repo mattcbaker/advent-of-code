c14dc26a0f65f434aae34e5242428d84ad742d6f
Matt Baker <baker.mattc@gmail.com>
2018-12-03 00:17:08 -0700

    2018, day 3

--- 2018/3/1.go
+++ 2018/3/1.go
@@ -1,3 +1,5 @@
+// wish I had time to clean this up, but sadly the next advent day is coming up so soon!!
+
 package main
 
 import (
@@ -16,7 +18,7 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		processClaim(scanner.Text(), grid)
+		readInClaim(scanner.Text(), grid)
 	}
 
 	overlap := 0
@@ -30,15 +32,13 @@ func main() {
 	fmt.Printf("first challenge: %d\n", overlap)
 }
 
-func processClaim(claim string, grid map[string]int) {
+func readInClaim(claim string, grid map[string]int) {
 	split := strings.Split(claim, " ")
 
 	left, _ := strconv.Atoi(strings.Split(split[2], ",")[0])
 	top, _ := strconv.Atoi(strings.TrimRight(strings.Split(split[2], ",")[1], ":"))
 	width, _ := strconv.Atoi(strings.Split(split[3], "x")[0])
 	height, _ := strconv.Atoi(strings.Split(split[3], "x")[1])
-
-	// fmt.Printf("id=%s, left=%d, top=%d, width=%d, height=%d\n", id, left, top, width, height)
 
 	for i := 0; i < width; i++ {
 		for j := 0; j < height; j++ {
